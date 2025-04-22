e525118aa6c19f9ce8ccbceef1210edad954497d
t4lhaa <muhammadtalha895c@gmail.com>
2025-04-22 17:23:21 +0500

    Update LectureCourse.cpp

--- Lab 9/task3/LectureCourse.cpp
+++ Lab 9/task3/LectureCourse.cpp
@@ -4,14 +4,12 @@ using namespace std;
 
 LectureCourse::LectureCourse(string courseCode, int credits) : Course(courseCode, credits) {}
 
-void LectureCourse::calculateGrade(string grade)
-{
-    cout << "Grade : " << grade << endl;
-}
-
 void LectureCourse::displayInfo()
 {
     cout << "Lecture Course: " << courseCode << " | Credits: " << credits << endl;
 }
 
-
+void LectureCourse::calculateGrade(string grade)
+{
+    cout << "Grade : " << grade << endl;
+}
