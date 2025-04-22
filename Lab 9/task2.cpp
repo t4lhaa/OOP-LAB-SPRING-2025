8e92df106455ed56ea786a0c13f63364c625e6ee
t4lhaa <muhammadtalha895c@gmail.com>
2025-04-22 14:32:18 +0500

    Create task2.cpp

---
+++ Lab 9/task2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+class MenuItem
+{
+protected:
+    string dishName;
+    double price;
+
+public:
+    MenuItem(string name, double price) : dishName(name), price(price)
+    {
+    }
+    virtual void showDetails() = 0;
+    virtual void prepare() = 0;
+};
+class Appetizer : public MenuItem
+{
+public:
+    Appetizer(string name, double price) : MenuItem(name, price)
+    {
+    }
+    void showDetails()
+    {
+        cout << "Appetizer Dish Name: " << dishName << endl;
+        cout << "Price: " << price << endl;
+    }
+    void prepare()
+    {
+        cout << "Appetizer " << dishName << " is preparing!!!" << endl;
+    }
+};
+class Maincourse : public MenuItem
+{
+public:
+    Maincourse(string name, double price) : MenuItem(name, price)
+    {
+    }
+    void showDetails()
+    {
+        cout << "Main Dish Name: " << dishName << endl;
+        cout << "Price: " << price << endl;
+    }
+    void prepare()
+    {
+        cout << "main " << dishName << " is preparing!!!" << endl;
+    }
+};
+int main()
+{
+    Appetizer appetizer1("Burger", 849);
+    Maincourse maincourse1("Pizza", 1949);
+
+    MenuItem *d1;
+    d1 = &appetizer1;
+    d1->showDetails();
+    d1->prepare();
+
+    d1 = &maincourse1;
+    d1->showDetails();
+    d1->prepare();
+}
