3fcf61c125e8d833ee779721eadcfed7e19445c3
t4lhaa <muhammadtalha895c@gmail.com>
2025-04-22 17:15:06 +0500

    Create LabCourse.cpp

---
+++ Lab 9/task3/LabCourse.cpp
@@ -0,0 +1,15 @@
+#include "LabCourse.h"
+#include <iostream>
+using namespace std;
+
+LabCourse::LabCourse(string courseCode, int credits) : Course(courseCode, credits) {}
+
+void LabCourse::displayInfo()
+{
+    cout << "Lab Course: " << courseCode << " | Credits: " << credits << endl;
+}
+
+void LabCourse::calculateGrade()
+{
+    cout << "Final Grade of Lab:80 " << endl;
+}
